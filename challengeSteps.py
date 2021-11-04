141be842a2eadecbb4661f9d4a2b68a9a549c603
barif <barifergan10@gmail.com>
2021-11-04 09:50:56 +0200

    step 1- need to improve shortest distance

--- challengeSteps.py
+++ challengeSteps.py
@@ -1,13 +1,13 @@
 import math
+import sys
+
 import numpy as np
 import matplotlib.pyplot as plt
 import utm
 import ast
-from scipy.spatial import distance
 
 
 def step_0(path):
-
     network, parcels = get_lists_from_file(path)
 
     utm_network = convert_to_utm(network)
@@ -16,8 +16,8 @@ def step_0(path):
     utm_parcels = np.array(utm_parcels, dtype=object)
     utm_network = np.array(utm_network, dtype=object)
 
-    plot_data(utm_network, 'r')
-    plot_data(utm_parcels, 'b')
+    plot_data(utm_network, False, 'r')
+    plot_data(utm_parcels, False, 'b')
     plt.show()
 
     return utm_network, utm_parcels
@@ -53,20 +53,117 @@ def convert_to_utm(list_to_convert):
             as_utm = utm.from_latlon(lat, lng)
             x = as_utm[0]
             y = as_utm[1]
-            # as_utm = pyproj.Proj(proj='utm', zone=36, ellps='WGS84')
-            # x, y = as_utm(lat, lng)
-            #
             utm_inside_list.append([x, y])
         utm_list.append(utm_inside_list)
 
     return utm_list
 
 
-def plot_data(data, color):
-    for array in data:
-        array = np.array(array, dtype=object)
-        x, y = array.T
+def plot_data(data, is_edge, color):
+    if data is None:
+        return
+    if is_edge:
+        x, y = data.T
         plt.plot(x, y, color)
+    else:
+        for array in data:
+            array = np.array(array, dtype=object)
+            x, y = array.T
+            plt.plot(x, y, color)
+
+
+def step_1(network, parcels):
+    # TODO: fix distance between 2 edges!!
+
+    plot_data(network, False, 'r')
+    plot_data(parcels, False, 'b')
+    # for parcel in parcels:
+    #     for i in range(0, len(parcel) - 1):
+    #         parcel_edge = parcel[i], parcel[i + 1]
+    #         for net in network:
+    #             shortest_distance = sys.maxsize
+    #             closest_edge = None
+    #             for j in range(0, len(net) - 1):
+    #                 network_edge = net[j], net[j + 1]
+    #                 dist = get_two_lines_distance(parcel_edge, network_edge)
+    #                 if dist <= 20:
+    #                     if dist < shortest_distance:
+    #                         shortest_distance = dist
+    #                         closest_edge = parcel_edge
+    #                         closest_edge = np.array(closest_edge, dtype=object)
+    #             plot_data(closest_edge, True, 'g')
+
+    for net in network:
+        for i in range(0, len(net) - 1):
+            network_edge = net[i], net[i + 1]
+            for parcel in parcels:
+                closest_edge = None
+                shortest_distance = sys.maxsize
+                for j in range(0, len(parcel) - 1):
+                    parcel_edge = parcel[j], parcel[j + 1]
+                    dist = get_two_lines_distance(parcel_edge, network_edge)
+                    if dist <= 20 and dist < shortest_distance:
+                        shortest_distance = dist
+                        closest_edge = parcel_edge
+                        closest_edge = np.array(closest_edge, dtype=object)
+                plot_data(closest_edge, True, 'g')
+
+    plt.show()
+
+
+def get_two_lines_distance(edge1, edge2):
+
+    distances = []
+
+    # edge1 endpoints
+    p1 = edge1[0]
+    p2 = edge1[1]
+
+    # edge 2 endpoints
+    p3 = edge2[0]
+    p4 = edge2[1]
+
+    # midpoints
+    midpoint1 = (p1[0] + p2[0])/2, (p1[1] + p2[1])/2
+    midpoint2 = (p3[0] + p4[0])/2, (p3[1] + p4[1])/2
+
+    edge1_points = [p1, midpoint1, p2]
+    edge2_points = [p3, midpoint2, p4]
+
+    for e1_p in edge1_points:
+        for e2_p in edge2_points:
+            dist = euclidean_distance(e1_p[0], e2_p[0], e1_p[1], e2_p[1])
+            distances.append(dist)
+
+    shortest_dist = min(distances)
+    return shortest_dist
+
+
+def euclidean_distance(x1, x2, y1, y2):
+    pow_x = math.pow((x1 - x2), 2)
+    pow_y = math.pow((y1 - y2), 2)
+    dist = math.sqrt(pow_x + pow_y)
+    return dist
+
+
+def get_edge_length(edge):
+    p1 = edge[0]
+    p2 = edge[1]
+
+    x1 = p1[0]
+    x2 = p2[0]
+    y1 = p1[1]
+    y2 = p2[1]
+
+    pow_x = math.pow((x1 - x2), 2)
+    pow_y = math.pow((y1 - y2), 2)
+    dist = math.sqrt(pow_x + pow_y)
+    return dist
+
+
+def step_2():
+    pass
+
 
 network_array, parcels_array = step_0('python-challenge.txt')
-
+step_1(network_array, parcels_array)
